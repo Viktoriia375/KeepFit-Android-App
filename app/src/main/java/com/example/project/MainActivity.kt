b33d618c8cab4461cb976a3dabce3253b3314ecb
Viktoriia <viktoriia.diak5@gmail.com>
2022-05-04 20:55:38 +0300

    third commit

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.project
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,8 +11,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_menu)
+        setContentView(R.layout.create_account_menu)
+        setContentView(R.layout.main_menu)
 
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.bottom_nav_menu, menu)
+        return true
     }
 
 }
